5bd4452f12aa43633da1be1515500c8badc6da68
Cerberus <51792843+Cerberus22@users.noreply.github.com>
2022-08-27 12:39:52 +0200

    Add files via upload

--- RSATank/Optical.ino
+++ RSATank/Optical.ino
@@ -1,8 +1,8 @@
 /*
  * Pins used for the infrared sensors
  */
-#define PIN_OPTICAL_LEFT 8
-#define PIN_OPTICAL_RIGHT 9
+#define PIN_OPTICAL_LEFT 9
+#define PIN_OPTICAL_RIGHT 8
 
 // Setting up the pins related to the optical sensor. But pins are digital inputs.
 void OpticalSetup (void)
--- RSATank/RSATank.ino
+++ RSATank/RSATank.ino
@@ -2,6 +2,25 @@
  * Please do not change the pin numbers, feel free to change the name of the constants!
  */
 
+#define PIN_OPTICAL_LEFT 9
+#define PIN_OPTICAL_RIGHT 8
+
+#define PIN_ENABLE_LEFT  24
+#define PIN_REVERSE_LEFT 7
+#define PIN_FORWARD_LEFT 6
+
+#define PIN_ENABLE_RIGHT  25
+#define PIN_REVERSE_RIGHT 3
+#define PIN_FORWARD_RIGHT 2
+
+#define NO_OF_DIRECTIONS 4
+
+#define MOTOR_CODE_RESET    0b0000
+#define MOTOR_CODE_FORWARD  0b1100
+#define MOTOR_CODE_REVERSE  0b0011
+#define MOTOR_CODE_LEFT     0b0110
+#define MOTOR_CODE_RIGHT    0b1001
+
 /*
  * This function runs once and we have already implemented everything needed 
  * for the motors and the starting sensors. Be careful if you add/remove any
@@ -14,7 +33,8 @@ void setup ()
   MotorSetup();
   SonarSetup();
   OpticalSetup();
-//  CommunicationSetup();
+  CommunicationSetup();
+  Serial.begin(9600);
 }
 
 /*
@@ -22,7 +42,7 @@ void setup ()
  */
 void loop () 
 {
-  delay(100)
+  Serial.println(digitalRead(PIN_OPTICAL_LEFT));
 //  CommunicationUpdate();
 //  CommunicationCheck();
 }
